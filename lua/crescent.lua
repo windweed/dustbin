37dac7325f8e7e51cd2d3c6edd71705309660999
czy <ziijchen@163.com>
2021-06-09 21:48:24 +0800

    add crescent.lua

---
+++ lua/crescent.lua
@@ -0,0 +1,31 @@
+#! /usr/bin/env lua
+
+function disk(cx, cy, r) -- create a circle
+    return function (x, y) return (x - cx)^2 + (y - cy)^2 <= r^2 end
+end
+
+function difference(r1, r2)
+    return function(x, y) return r1(x, y) and not r2(x, y) end
+end
+
+function translate(r, dx, dy)
+    return function (x, y) return r(x -dx, y - dy) end
+end
+
+function plot(r, M, N)
+    io.write("P1\n", M, " ", N, "\n")
+    for i = 1, N do
+        local y = (N - i*2) / N
+        for j = 1, M do
+            local x = (j*2 - M) / M
+            io.write(r(x, y) and "1" or "0")
+        end
+        io.write("\n")
+    end
+end
+
+
+io.output("a.pbm")
+c1 = disk(0, 0, 1)
+plot(difference(c1, translate(c1, 0.3, 0)), 500, 500)
+
