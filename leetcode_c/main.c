003f9f0f1740a8d0f2530aecf2d3fb685440e9bf
swingczy <138293563+swingczy@users.noreply.github.com>
2024-02-26 12:48:05 +0000

    buildup leetcode proj

---
+++ leetcode_c/main.c
@@ -0,0 +1,24 @@
+#include "treenode.h"
+#include <stdio.h>
+
+int dfs(struct TreeNode *node, int low, int high) {
+    if (!node) {
+        return 0;
+    }
+    if (node->val < low) {
+        return dfs(node->right, low, high);
+    } else if (node->val > high) {
+        return dfs(node->left, low, high);
+    } else {
+        return node->val + dfs(node->left, low, high) + dfs(node->right, low, high);
+    }
+}
+
+int rangeSumBST(struct TreeNode* root, int low, int high) {
+    return dfs(root, low, high);
+}
+
+int main() {
+    printf("Hello C\n");
+    return 0;
+}
